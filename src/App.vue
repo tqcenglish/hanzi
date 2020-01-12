cf8eaf335c2584cdd6eaeed90763316dd99313b9
tqcenglish <tqcenglish@gmail.com>
2020-01-12 17:10:51 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="汉字动画"/>
+    <HanZi msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HanZi from './components/HanZi.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    HanZi,
   }
 }
 </script>
---
+++ src/components/HanZi.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div id="character-target-div"></div>
+    <button @click="animate()">动起来</button>
+  </div>
+</template>
+
+<script>
+const HanziWriter = require('hanzi-writer');
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      writer: null,
+    }
+  },
+  mounted: function name() {
+    this.writer = HanziWriter.create('character-target-div', '唐', {
+      width: 100,
+      height: 100,
+      padding: 5
+    });
+  },
+  methods: {
+    animate: function () {
+      this.writer.animateCharacter();
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
